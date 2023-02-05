1ee0a0a2ef02fabbd65281ad59ab9bc08694d629
ItisNoMatter <702sunnylife@gmail.com>
2023-02-05 16:32:47 +0900

    tera導入

--- rust-app/src/main.rs
+++ rust-app/src/main.rs
@@ -1,18 +1,25 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-
+use actix_web::{error, get, web, App, Error, HttpResponse, HttpServer, Responder};
+use tera::Tera;
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn hello(templates: web::Data<Tera>) -> impl Responder {
+    let view = templates.render("index.html", &tera::Context::new());
+
+    match view {
+        Ok(body) => HttpResponse::Ok().content_type("text/html").body(body),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+    }
 }
 
 async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+    HttpResponse::Ok().body("Hey there!!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let templates = Tera::new("templates/**/*").unwrap();
         App::new()
+            .data(templates)
             .service(hello)
             .route("/hey", web::get().to(manual_hello))
     })
