e39e6b62c5e2d9c01f5eb51541e9c51f927ee374
Aruminium <b2201680@photon.chitose.ac.jp>
2023-02-06 01:30:13 +0900

    Top画面のモック作成(#2)

--- rust-app/src/main.rs
+++ rust-app/src/main.rs
@@ -11,10 +11,6 @@ async fn hello(templates: web::Data<Tera>) -> impl Responder {
     }
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!!")
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -22,7 +18,6 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(Data::new(templates))
             .service(hello)
-            .route("/hey", web::get().to(manual_hello))
     })
     .bind(("rust-app", 8080))?
     .run()
