7d2ae51e9485ae97a7abd199c3e3389d21006263
ItisNoMatter <702sunnylife@gmail.com>
2023-02-08 22:04:39 +0900

    postをさばく実装例を追加

--- rust-app/src/main.rs
+++ rust-app/src/main.rs
@@ -1,6 +1,13 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use actix_web::web::Data;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
 use tera::Tera;
+
+#[derive(Deserialize)]
+struct Example {
+    text: String,
+}
+
 #[get("/")]
 async fn hello(templates: web::Data<Tera>) -> impl Responder {
     let view = templates.render("index.html", &tera::Context::new());
@@ -10,7 +17,10 @@ async fn hello(templates: web::Data<Tera>) -> impl Responder {
         Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
     }
 }
-
+#[post("/post")]
+async fn post_example(item: web::Json<Example>) -> String {
+    format!("text: {}\n", item.text)
+}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -18,6 +28,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(Data::new(templates))
             .service(hello)
+            .service(post_example)
     })
     .bind(("rust-app", 8080))?
     .run()
