573f65c1dd05aa6bf19c6d8d4786043854806a26
ItisNoMatter <702sunnylife@gmail.com>
2023-02-09 16:32:15 +0900

    bffにpostを実装

--- rust-app/src/main.rs
+++ rust-app/src/main.rs
@@ -1,8 +1,8 @@
 use actix_web::web::Data;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use reqwest;
 use serde::Deserialize;
 use tera::Tera;
-
 #[derive(Deserialize)]
 struct Example {
     text: String,
@@ -18,9 +18,25 @@ async fn hello(templates: web::Data<Tera>) -> impl Responder {
     }
 }
 #[post("/post")]
-async fn post_example(item: web::Json<Example>) -> String {
-    format!("text: {}\n", item.text)
+async fn post_example(item: web::Json<Example>) -> impl Responder {
+    format!("responce is \n",);
+    let res = send_post_to_bff(&item.text);
+    match res.await {
+        Ok(res) => HttpResponse::Ok().content_type("text/html").body(res),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+    }
 }
+
+async fn send_post_to_bff(body: &String) -> reqwest::Result<String> {
+    let client = reqwest::Client::new();
+    let responce = client
+        .post("http://localhost:9000/graphql")
+        .body(format!("{{text: {}}}\n", body)) //ひどい実装ですみませんが動作確認なので許してほしいです
+        .send()
+        .await?;
+    responce.text().await
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
